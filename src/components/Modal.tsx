2925e1facb220d929fe669a8b671f575c5997714
delafin <lifinhime@gmail.com>
2023-01-23 04:02:00 +0300

    fixed typos and change state to ref

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,15 +4,13 @@ import { type Dispatch, type SetStateAction, useEffect, useState, useRef } from 
 import { useMethodName3Query } from '../store/variableApi';
 import useOutsideFunction from '../hooks/useOutsideFunction';
 
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay, A11y} from 'swiper';
+import { Navigation, Autoplay, A11y } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/a11y';
 
 import ReactDOM from 'react-dom';
-
 
 type TChildren = {
 	name: string;
@@ -24,70 +22,89 @@ type PChildren = {
 	children?: React.ReactNode;
 };
 
-
 const Modal: NextPage<TChildren> = ({ name, userId, setIsOpen, isOpen }) => {
+	const { data: variableFrom = [], isFetching, isLoading } = useMethodName3Query(userId);
 
-
-	const {
-		data: variableFrom = [],
-		isFetching,
-		isLoading,
-	} = useMethodName3Query(userId);
-
-	
 	const modalMain = useRef<HTMLDivElement | null>(null);
 	const onClose = () => {
-		setIsOpen(() => false)
-	}
-	useOutsideFunction(modalMain,onClose)
+		setIsOpen(() => false);
+	};
+	useOutsideFunction(modalMain, onClose);
 
 	const [isClient, setIsClient] = useState(false);
 	useEffect(() => {
-		setIsClient(() => true)
+		setIsClient(() => true);
 	}, []);
-	
+
 	useEffect(() => {
-		document.body.classList.add("overflow-y-hidden")
-		if(!isOpen){
-			return 	document.body.classList.remove("overflow-y-hidden")
+		document.body.classList.add('overflow-y-hidden');
+		if (!isOpen) {
+			return document.body.classList.remove('overflow-y-hidden');
 		}
 	}, [isOpen]);
 
 	const nextButton = useRef<HTMLButtonElement | null>(null);
 	const prevButton = useRef<HTMLButtonElement | null>(null);
+	const swiperRef = useRef(null);
+	// const [prevButton, setPrevButton] = useState<HTMLButtonElement | null>(null);
+	// const [nextButton, setNextButton] = useState<HTMLButtonElement | null>(null);
 
-	if(!isClient) {
+	if (!isClient) {
 		return null;
 	}
-	if(!isOpen) {
-		return null
+	if (!isOpen) {
+		return null;
 	}
-
 	return (
 		<Portal>
-			<div className='bg-black/50 flex justify-center items-center fixed top-0 left-0 right-0 bottom-0 ' >
-				<div className='max-w-lg 2md:w-full overflow-hidden rounded-xl bg-white p-4 shadow-xl top-1/2 left-1/2 -translate-1/2' ref={modalMain}>
+			<div className='fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black/50 '>
+				<div
+					className='-translate-1/2 top-1/2 left-1/2 max-w-lg overflow-hidden rounded-xl bg-white p-4 shadow-xl 2md:w-full'
+					ref={modalMain}
+				>
 					<div className='mt-3 flex flex-col items-center justify-center gap-3'>
 						<p className='text-center text-black'>{name.length > 20 ? `${name.slice(0, 20)}...` : name}</p>
 					</div>
 
+					<div className='mt-3 flex items-center justify-center gap-3'>
+						<button
+							className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+							ref={prevButton}
+							// ref={(newRef) => setPrevButton(newRef)}
+						>
+							Prev
+						</button>
+						<button
+							className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+							ref={nextButton}
+							// ref={(newRef) => setNextButton(newRef)}
+						>
+							Next
+						</button>
+					</div>
+
 					<Swiper
-						modules={[ Navigation, Autoplay, A11y]}
+						modules={[Navigation, Autoplay, A11y]}
 						navigation={{
-							prevEl: prevButton?.current,
-							nextEl: nextButton?.current
+							prevEl: prevButton.current,
+							nextEl: nextButton.current,
+							// prevEl: prevButton,
+							// nextEl: nextButton,
 						}}
 						onInit={(swiper) => {
 							if (typeof swiper.params.navigation !== 'boolean' && swiper.params.navigation) {
-								swiper.params.navigation.prevEl = prevButton?.current;
-								swiper.params.navigation.nextEl = nextButton?.current;
+								swiper.params.navigation.prevEl = prevButton.current;
+								swiper.params.navigation.nextEl = nextButton.current;
+								// swiper.params.navigation.prevEl = prevButton;
+								// swiper.params.navigation.nextEl = nextButton;
 								swiper.navigation.init();
 								swiper.navigation.update();
 							}
-						  }}
+						}}
 						grabCursor={true}
 						centeredSlides={true}
-						observer = {true}
+						observer={true}
+						observeParents={true}
 						keyboard={{
 							enabled: true,
 							onlyInViewport: false,
@@ -99,28 +116,28 @@ const Modal: NextPage<TChildren> = ({ name, userId, setIsOpen, isOpen }) => {
 						}}
 						spaceBetween={10}
 						slidesPerView={1}
+						ref={swiperRef}
 					>
-						{isLoading || isFetching ? <p className='text-black text-center w-full p-10 mx-auto'>Loading...</p> :
-						variableFrom ? variableFrom.map((item) => {
-							return (
-								<SwiperSlide className='' key={item.id}>
-									<Slide title={item.title}/>
-								</SwiperSlide>
-							);
-							}) : <p className='text-black text-center w-full p-10 mx-auto'>Please, select user</p>
-						}
+						{isLoading || isFetching ? (
+							<p className='mx-auto w-full p-10 text-center text-black'>Loading...</p>
+						) : variableFrom ? (
+							variableFrom.map((item) => {
+								return (
+									<SwiperSlide className='' key={item.id}>
+										<Slide title={item.title} />
+									</SwiperSlide>
+								);
+							})
+						) : (
+							<p className='mx-auto w-full p-10 text-center text-black'>Please, select user</p>
+						)}
+					</Swiper>
 
-						</Swiper>
-					<div className='mt-3 flex items-center justify-center gap-3'>
-						<button className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40' ref={prevButton} >
-							Prev
-						</button>
-						<button className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40' ref={nextButton} >
-							Next
-						</button>
-					</div>
 					<div className='mt-3 w-full'>
-						<button className=' mx-auto block transform rounded-md bg-blue-600 px-4 py-2 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40' onClick={() => setIsOpen(() => false)}>
+						<button
+							className=' mx-auto block transform rounded-md bg-blue-600 px-4 py-2 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+							onClick={() => setIsOpen(() => false)}
+						>
 							Close Modal
 						</button>
 					</div>
@@ -130,12 +147,12 @@ const Modal: NextPage<TChildren> = ({ name, userId, setIsOpen, isOpen }) => {
 	);
 };
 
-const Portal: React.FC<PChildren> = ({children}) => {
+const Portal: React.FC<PChildren> = ({ children }) => {
 	// const parent = document.createElement('div')
 	// document.body.append(parent)
-	const parent = document.body
-	document.body.classList.add("relative")
-	return ReactDOM.createPortal(children, parent)
-}
+	const parent = document.body;
+	document.body.classList.add('relative');
+	return ReactDOM.createPortal(children, parent);
+};
 
 export default Modal;
--- src/components/PostInfo.tsx
+++ src/components/PostInfo.tsx
@@ -1,12 +1,11 @@
 import { type NextPage } from 'next';
 
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay, A11y} from 'swiper';
+import { Navigation, Autoplay, A11y } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/a11y';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Slide from './Slide';
 
 type TChildren = {
@@ -16,68 +15,74 @@ type TChildren = {
 	userName: string | undefined;
 };
 
-const PostInfo: NextPage<TChildren> = ({variableFrom,isFetching,isLoading, userName}) => {
-
+const PostInfo: NextPage<TChildren> = ({ variableFrom, isFetching, isLoading, userName }) => {
 	const nextButton = useRef<HTMLButtonElement | null>(null);
 	const prevButton = useRef<HTMLButtonElement | null>(null);
-	
+
 	return (
 		<section className='mt-10'>
-
 			<div className='container'>
-    			<div className='block max-w-lg 2md:w-full rounded-xl bg-white p-4 shadow-xl mx-auto -translate-1/2'>
-				<p className='text-black text-center w-full p-1 mx-auto'>{userName}</p>
-			<Swiper
-				modules={[ Navigation, Autoplay, A11y]}
-
-				navigation={{
-					prevEl: prevButton?.current,
-					nextEl: nextButton?.current
-				}}
-				onInit={(swiper) => {
-					if (typeof swiper.params.navigation !== 'boolean' && swiper.params.navigation) {
-						swiper.params.navigation.prevEl = prevButton?.current;
-						swiper.params.navigation.nextEl = nextButton?.current;
-						swiper.navigation.init();
-						swiper.navigation.update();
-					}
-					}}
-				grabCursor={true}
-				centeredSlides={true}
-				observer = {true}
-				keyboard={{
-					enabled: true,
-					onlyInViewport: false,
-				}}
-				speed={1000}
-				autoplay={{
-					delay: 1000,
-					disableOnInteraction: false,
-				}}
-				spaceBetween={10}
-				slidesPerView={1}
-				className='mt-10'
-			>
-				{isLoading || isFetching ? <p className='text-black text-center w-full p-10 mx-auto'>Loading...</p> :
-				variableFrom ? variableFrom.map((item) => {
-					return (
-						<SwiperSlide className='' key={item.id}>
-							<Slide content={item.body}  title={item.title}/>
-						</SwiperSlide>
-					);
-					}) : <p className='text-black text-center w-full p-10 mx-auto'>Please, select user</p>
-				}
-			</Swiper>
-			<div className='mt-3 flex items-center justify-center gap-3'>
-						<button className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40' ref={prevButton} >
+				<div className='-translate-1/2 mx-auto block max-w-lg rounded-xl bg-white p-4 shadow-xl 2md:w-full'>
+					<p className='mx-auto w-full p-1 text-center text-black'>{userName}</p>
+					<div className='mt-3 flex items-center justify-center gap-3'>
+						<button
+							className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+							ref={prevButton}
+						>
 							Prev
 						</button>
-						<button className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40' ref={nextButton} >
+						<button
+							className='block transform rounded-md bg-blue-600 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+							ref={nextButton}
+						>
 							Next
 						</button>
 					</div>
-
-     	</div>
+					<Swiper
+						modules={[Navigation, Autoplay, A11y]}
+						navigation={{
+							prevEl: prevButton?.current,
+							nextEl: nextButton?.current,
+						}}
+						onInit={(swiper) => {
+							if (typeof swiper.params.navigation !== 'boolean' && swiper.params.navigation) {
+								swiper.params.navigation.prevEl = prevButton?.current;
+								swiper.params.navigation.nextEl = nextButton?.current;
+								swiper.navigation.init();
+								swiper.navigation.update();
+							}
+						}}
+						grabCursor={true}
+						centeredSlides={true}
+						observer={true}
+						keyboard={{
+							enabled: true,
+							onlyInViewport: false,
+						}}
+						speed={1000}
+						autoplay={{
+							delay: 1000,
+							disableOnInteraction: false,
+						}}
+						spaceBetween={10}
+						slidesPerView={1}
+						className='mt-10'
+					>
+						{isLoading || isFetching ? (
+							<p className='mx-auto w-full p-10 text-center text-black'>Loading...</p>
+						) : variableFrom ? (
+							variableFrom.map((item) => {
+								return (
+									<SwiperSlide className='' key={item.id}>
+										<Slide content={item.body} title={item.title} />
+									</SwiperSlide>
+								);
+							})
+						) : (
+							<p className='mx-auto w-full p-10 text-center text-black'>Please, select user</p>
+						)}
+					</Swiper>
+				</div>
 			</div>
 		</section>
 	);
